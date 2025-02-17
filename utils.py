9df3daf7799d4d308327d0f0eda868f0deb3be6d
jwhao <107764295+jwhao@users.noreply.github.com>
2025-02-17 11:12:24 +0800

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,331 @@
+import os
+import shutil
+import time
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.optim import SGD, Adam
+from torch.optim.lr_scheduler import MultiStepLR
+import tqdm
+
+_log_path = None
+
+def set_log_path(path):
+    global _log_path
+    _log_path = path
+
+def log(obj, filename='log.txt'):
+    print(obj)
+    if _log_path is not None:
+        with open(os.path.join(_log_path, filename), 'a') as f:
+            print(obj, file=f)
+
+
+class Averager():
+
+    def __init__(self):
+        self.n = 0.0
+        self.v = 0.0
+
+    def add(self, v, n=1.0):
+        self.v = (self.v * self.n + v * n) / (self.n + n)
+        self.n += n
+
+    def item(self):
+        return self.v
+
+
+class Timer():
+
+    def __init__(self):
+        self.v = time.time()
+
+    def s(self):
+        self.v = time.time()
+
+    def t(self):
+        return time.time() - self.v
+
+
+def set_gpu(gpu):
+    print('set gpu:', gpu)
+    os.environ['CUDA_VISIBLE_DEVICES'] = gpu
+
+
+def ensure_path(path, remove=True):
+    basename = os.path.basename(path.rstrip('/'))
+    if os.path.exists(path):
+        if remove and (basename.startswith('_')
+                or input('{} exists, remove? ([y]/n): '.format(path)) != 'n'):
+            shutil.rmtree(path)
+            os.makedirs(path)
+    else:
+        os.makedirs(path)
+
+
+def time_str(t):
+    if t >= 3600:
+        return '{:.1f}h'.format(t / 3600)
+    if t >= 60:
+        return '{:.1f}m'.format(t / 60)
+    return '{:.1f}s'.format(t)
+
+
+def compute_logits(feat, proto, metric='dot', temp=1.0):
+    assert feat.dim() == proto.dim()
+
+    if feat.dim() == 2:
+        if metric == 'dot':
+            logits = torch.mm(feat, proto.t())
+        elif metric == 'cos':
+            logits = torch.mm(F.normalize(feat, dim=-1),
+                              F.normalize(proto, dim=-1).t()) 
+        elif metric == 'sqr':
+            logits = -(feat.unsqueeze(1) -
+                       proto.unsqueeze(0)).pow(2).sum(dim=-1)
+
+    elif feat.dim() == 3:
+        if metric == 'dot':
+            logits = torch.bmm(feat, proto.permute(0, 2, 1))
+        elif metric == 'cos':
+            logits = torch.bmm(F.normalize(feat, dim=-1),
+                               F.normalize(proto, dim=-1).permute(0, 2, 1))
+        elif metric == 'sqr':
+            logits = -(feat.unsqueeze(2) -
+                       proto.unsqueeze(1)).pow(2).sum(dim=-1)
+
+    return logits * temp
+
+
+def compute_acc(logits, label, reduction='mean'):
+    ret = (torch.argmax(logits, dim=1) == label).float()
+    if reduction == 'none':
+        return ret.detach()
+    elif reduction == 'mean':
+        return ret.mean().item()
+
+
+def compute_n_params(model, return_str=True):
+    tot = 0
+    for p in model.parameters():
+        w = 1
+        for x in p.shape:
+            w *= x
+        tot += w
+    if return_str:
+        if tot >= 1e6:
+            return '{:.1f}M'.format(tot / 1e6)
+        else:
+            return '{:.1f}K'.format(tot / 1e3)
+    else:
+        return tot
+
+
+def make_optimizer(params, name, lr, weight_decay=None, milestones=None):
+    if weight_decay is None:
+        weight_decay = 0.
+    if name == 'sgd':
+        optimizer = SGD(params, lr, momentum=0.9, weight_decay=weight_decay)
+    elif name == 'adam':
+        optimizer = Adam(params, lr, weight_decay=weight_decay)
+    if milestones:
+        lr_scheduler = MultiStepLR(optimizer, milestones)
+    else:
+        lr_scheduler = None
+    return optimizer, lr_scheduler
+
+
+def visualize_dataset(dataset, name, writer, n_samples=16):
+    demo = []
+    for i in np.random.choice(len(dataset), n_samples):
+        demo.append(dataset.convert_raw(dataset[i][0]))
+    writer.add_images('visualize_' + name, torch.stack(demo))
+    writer.flush()
+
+
+def freeze_bn(model):
+    for m in model.modules():
+        if isinstance(m, nn.BatchNorm2d):
+            m.eval()
+
+
+
+def cls_acc(output, target, topk=1):
+    pred = output.topk(topk, 1, True, True)[1].t()
+    correct = pred.eq(target.view(1, -1).expand_as(pred))
+    acc = float(correct[: topk].reshape(-1).float().sum(0, keepdim=True).cpu().numpy())
+    acc = 100 * acc / target.shape[0]
+    return acc
+
+
+def build_cache_model(cfg, clip_model, train_loader_cache):
+
+    if cfg['load_cache'] == False:    
+        cache_keys = []
+        cache_values = []
+
+        with torch.no_grad():
+            # Data augmentation for the cache model
+            for augment_idx in range(cfg['augment_epoch']):
+                train_features = []
+
+                print('Augment Epoch: {:} / {:}'.format(augment_idx, cfg['augment_epoch']))
+                for i, (images, target) in enumerate(tqdm(train_loader_cache)):
+                    images = images.cuda()
+                    image_features = clip_model.encode_image(images)
+                    train_features.append(image_features)
+                    if augment_idx == 0:
+                        target = target.cuda()
+                        cache_values.append(target)
+                cache_keys.append(torch.cat(train_features, dim=0).unsqueeze(0))
+            
+        cache_keys = torch.cat(cache_keys, dim=0).mean(dim=0)
+        cache_keys /= cache_keys.norm(dim=-1, keepdim=True)
+        cache_keys = cache_keys.permute(1, 0)
+        cache_values = F.one_hot(torch.cat(cache_values, dim=0)).half()
+
+        torch.save(cache_keys, cfg['cache_dir'] + '/keys_' + str(cfg['shots']) + "shots.pt")
+        torch.save(cache_values, cfg['cache_dir'] + '/values_' + str(cfg['shots']) + "shots.pt")
+
+    else:
+        cache_keys = torch.load(cfg['cache_dir'] + '/keys_' + str(cfg['shots']) + "shots.pt")
+        cache_values = torch.load(cfg['cache_dir'] + '/values_' + str(cfg['shots']) + "shots.pt")
+
+    return cache_keys, cache_values
+
+
+def pre_load_features(cfg, split, clip_model, loader):
+
+    if cfg['load_pre_feat'] == False:
+        features, labels = [], []
+
+        with torch.no_grad():
+            for i, (images, target) in enumerate(tqdm(loader)):
+                images, target = images.cuda(), target.cuda()
+                image_features = clip_model.encode_image(images)
+                image_features /= image_features.norm(dim=-1, keepdim=True)
+                features.append(image_features)
+                labels.append(target)
+
+        features, labels = torch.cat(features), torch.cat(labels)
+
+        torch.save(features, cfg['cache_dir'] + "/" + split + "_f.pt")
+        torch.save(labels, cfg['cache_dir'] + "/" + split + "_l.pt")
+   
+    else:
+        features = torch.load(cfg['cache_dir'] + "/" + split + "_f.pt")
+        labels = torch.load(cfg['cache_dir'] + "/" + split + "_l.pt")
+    
+    return features, labels
+
+
+def search_hp(cfg, cache_keys, cache_values, features, labels, clip_weights, adapter=None):
+
+    if cfg['search_hp'] == True:
+    
+        beta_list = [i * (cfg['search_scale'][0] - 0.1) / cfg['search_step'][0] + 0.1 for i in range(cfg['search_step'][0])]
+        alpha_list = [i * (cfg['search_scale'][1] - 0.1) / cfg['search_step'][1] + 0.1 for i in range(cfg['search_step'][1])]
+
+        best_acc = 0
+        best_beta, best_alpha = 0, 0
+
+        for beta in beta_list:
+            for alpha in alpha_list:
+                if adapter:
+                    affinity = adapter(features)
+                else:
+                    affinity = features @ cache_keys
+
+                cache_logits = ((-1) * (beta - beta * affinity)).exp() @ cache_values
+                clip_logits = 100. * features @ clip_weights
+                tip_logits = clip_logits + cache_logits * alpha
+                acc = cls_acc(tip_logits, labels)
+            
+                if acc > best_acc:
+                    print("New best setting, beta: {:.2f}, alpha: {:.2f}; accuracy: {:.2f}".format(beta, alpha, acc))
+                    best_acc = acc
+                    best_beta = beta
+                    best_alpha = alpha
+
+        print("\nAfter searching, the best accuarcy: {:.2f}.\n".format(best_acc))
+
+    return best_beta, best_alpha
+
+def patchify(imgs, patch_size=16):
+        """
+        imgs: (N, 3, H, W)
+        x: (N, L, patch_size**2 *3)
+        """
+        p = patch_size
+        assert imgs.shape[2] == imgs.shape[3] and imgs.shape[2] % p == 0
+
+        h = w = imgs.shape[2] // p
+        x = imgs.reshape(shape=(imgs.shape[0], 3, h, p, w, p))
+        x = torch.einsum('nchpwq->nhwpqc', x)
+        x = x.reshape(shape=(imgs.shape[0], h * w, p**2 * 3))
+        return x
+
+def unpatchify(x, patch_size=16):
+    """
+    x: (N, L, patch_size**2 *3)
+    imgs: (N, 3, H, W)
+    """
+    p = patch_size
+    h = w = int(x.shape[1]**.5)
+    assert h * w == x.shape[1]
+    
+    x = x.reshape(shape=(x.shape[0], h, w, p, p, 3))
+    x = torch.einsum('nhwpqc->nchpwq', x)
+    imgs = x.reshape(shape=(x.shape[0], 3, h * p, h * p))
+    return imgs
+
+def random_masking(x, mask_ratio=0.5):
+    """
+    Perform per-sample random masking by per-sample shuffling.
+    Per-sample shuffling is done by argsort random noise.
+    x: [N, L, D], sequence
+    """
+    N, L, D = x.shape  # batch, length, dim
+    len_keep = int(L * (1 - mask_ratio))
+    
+    noise = torch.rand(N, L, device=x.device)  # noise in [0, 1]
+    
+    # sort noise for each sample
+    ids_shuffle = torch.argsort(noise, dim=1)  # ascend: small is keep, large is remove
+    ids_restore = torch.argsort(ids_shuffle, dim=1)
+
+    # keep the first subset
+    ids_keep = ids_shuffle[:, :len_keep]
+    x_masked = torch.gather(x, dim=1, index=ids_keep.unsqueeze(-1).repeat(1, 1, D))
+
+    # ids_keep = ids_shuffle[:, len_keep:L]
+    # y_masked = torch.gather(y, dim=1, index=ids_keep.unsqueeze(-1).repeat(1, 1, D))
+
+    # generate the binary mask: 0 is keep, 1 is remove
+    mask = torch.ones([N, L], device=x.device)
+    mask[:, :len_keep] = 0
+    # unshuffle to get the binary mask
+    mask = torch.gather(mask, dim=1, index=ids_restore)
+
+    return x_masked, mask, ids_restore
+
+def random_mask(x, mask_ratio=0.75):
+    """
+    Perform per-sample random masking by per-sample shuffling.
+    Per-sample shuffling is done by argsort random noise.
+    x: [B, C, H, W]
+    y: [B, C, h, w]
+    """
+    # patch
+    x = patchify(x)         # [B, C, H, W] -> [B, 196,768]
+
+    # random mask
+    x, _ , _ = random_masking(x,mask_ratio=mask_ratio)
+
+    # unpatch
+
+    x = unpatchify(x)
+
+    return x
